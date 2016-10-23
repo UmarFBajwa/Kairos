5f25f572682828c6cd839e69f9a518e8132e5898
Raymond Sapida <ray.sapida@gmail.com>
2016-10-22 20:32:48 -0700

    Remove extra call to getCardInformations from ClessDetectedEvent

--- js/app.js
+++ js/app.js
@@ -104,11 +104,11 @@ function pollContactLess(){
   .connect() // Connect to service
   .close() // Close service
   .open() // Open service
-  .on('ClessDetectedEvent', function (r) { // Listen to ClessDetectedEvent
-    console.log('Card detected');
-    console.log('Detected: ', r);
-    return getCardInformations();
-  })
+  // .on('ClessDetectedEvent', function (r) { // Listen to ClessDetectedEvent
+  //   console.log('Card detected');
+  //   console.log('Detected: ', r);
+  //   return getCardInformations();
+  // })
     .call('StartDetection', {data: {timeout: 10000}, success: detectContact, error: logResponse}) // Call start detection method
     // .then(function (r) {
     //   console.log('Please approach your card: ', r); 
